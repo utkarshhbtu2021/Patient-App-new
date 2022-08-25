19780572643a7067137bb89356b3b8fd7ce265aa
sahil <sahilarora.2567@gmail.com>
2022-08-25 10:51:30 +0530

    ds

--- my-app/src/component/review_appointment/index.js
+++ my-app/src/component/review_appointment/index.js
@@ -36,7 +36,8 @@ const ReviewAppoinment = () => {
           api
             .putAuth(request)
             .then((data) => {
-            console.log(data)
+            console.log("success data",data)
+            alert("Appointment booked")
             })
             .catch((error) => {})
     }
@@ -44,41 +45,240 @@ const ReviewAppoinment = () => {
     {
         console.log(a ,"a data ")
     }
-        <p>Review Appoinment</p>
-        <div>
-            <div>
-            <reCAPTCHA />
-                <p> Patient Information</p>
-                <div>
-                    <p> Ashok kumar </p>
-                    <p>{a.patientDetails.firstname}</p>
+             <div className="container-fluid">
 
-                    <p>
-                        Phone
-                    </p>
-                    <p>{a.patientDetails.phone}</p>
-                    <p> Email </p>
-                    <p>{a.patientDetails.email}</p>
-                </div>
-            </div>
-            <div>
-                <p>Appoinment Details</p>
-                <p>COVID-19 Testing</p>
-                <p>Day and Time </p>
-                <p>{a.patientDetails.value} {a.patientDetails.timeData}</p>
-                <p>Loction</p>
-                <p>{a.patientDetails.location}</p>
-                <p>Reason for Visit </p>
-                <p>{a.patientDetails.reason}</p>
-                <p>Insurance</p>
-                <p>{a.patientDetails.insurance}</p>
-                <p>Additional notes </p>
-                <p>{a.patientDetails.additional}</p>
-            
-            </div>
-        </div>
-        <button onClick={()=>{Preview()}}>Preview </button>
-        <button onClick={()=>{ShaduleAppointment()}}> Scheduled</button>
+<div className="row">
+	<div className="col-md-10 col-12 mx-auto">
+		<div className="card mb-1">
+			<div className="card-header bg-primary text-white text-large">Review Appointment</div>
+
+</div>
+		<div className="row">
+	<div className="col-md-6">
+		<div className="card mb-5 vh-80">
+			<div className="card-header text-medium bg-danger"><strong>Patient Information</strong></div>
+<div className="card-body">	
+	<div className="row mb-3">
+	<div className="col-md-12">	
+		<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-person-fill"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>{a.patientDetails.firstname + " "+a.patientDetails.lastname}</strong></div>
+<div className="">42 yr | 01-01-1980 |Male</div>
+
+</div>
+</div>
+</div>
+</div>
+		</div>
+	</div>
+	<hr/>
+	<div className="row mb-3">
+	<div className="col-md-12">	
+		<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-telephone-fill"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Phone</strong></div>
+<div className="">{a.patientDetails.phone}</div>
+
+</div>
+</div>
+</div>
+</div>
+		</div>
+	</div>
+	
+	<hr/>
+	<div className="row mb-3">
+	<div className="col-md-12">	
+		<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-envelope-fill"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Email</strong></div>
+<div className="">{a.patientDetails.email}</div>
+
+</div>
+</div>
+</div>
+</div>
+		</div>
+	</div>
+	
+</div>
+</div>
+		</div>
+			
+		<div className="col-md-6">
+		<div className="card mb-5 vh-80">
+			<div className="card-header text-medium bg-danger"><strong>Appointment Details</strong></div>
+<div className="card-body">	
+	<div className="row mb-3">
+	<div className="col-md-6">
+		<div className="card h-100 hover-scale-up bg-light">
+<a className="card-body text-center d-flex flex-row align-items-center" href="#">
+<div className="sw-8 sh-8 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+	<img src="img/provider-black.png" alt=""/> </div>
+<p className="heading ps-3 text-body mb-0"><strong>Covid-19 Testing</strong></p>
+
+</a>
+</div>
+		</div>
+	</div>
+	
+	<div className="row g-2 mb-3">
+<div className="col-xl-6 col-12">
+<div className="card border border-primary">
+<div className="card-body">
+<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-calendar-week"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Day and Time</strong></div>
+<div className="">{a.patientDetails.value}  <br/>{a.patientDetails.timeData}(EDT)</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div className="col-xl-6 col-12">
+<div className="card border border-primary">
+<div className="card-body">
+<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-map-fill"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Location</strong></div>
+<div className="">{a.patientDetails.location}</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+	
+	<div className="row g-2 mb-3">
+<div className="col-xl-6 col-12">
+<div className="card border border-primary">
+<div className="card-body">
+<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-patch-question-fill"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Reason for Visit</strong></div>
+<div className="">{a.patientDetails.reason}</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div className="col-xl-6 col-12">
+<div className="card border border-primary">
+<div className="card-body">
+<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-shield-fill-check"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Insurance</strong></div>
+<div className="">{a.patientDetails.insurance}</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+	
+	<div className="row">
+	<div className="col-md-12">
+		<p className="mb-0"><strong>Additional Notes</strong></p>
+		<p>{a.patientDetails.additional}</p>
+		</div>
+	</div>	
+	
+	{/* <div className="row mb-3">
+	<div className="col-md-12">
+		Robot Capcha
+	
+		</div>
+	</div> */}
+	<div className="row mb-3">
+	<div className="col-md-12">
+		<input type="checkbox" className="" /> I agree to the <span className="text-primary">Terms and Conditions </span>and <span className="text-primary">Privacy policy.</span>
+	
+		</div>
+	</div>
+	<div className="row">	
+		<div className="col-12">
+			<button type="button" className="btn btn-light mb-1 me-3" onClick={()=>{Preview()}}>Previous</button>
+			<button type="button" className="btn btn-primary mb-1" onClick={()=>{ShaduleAppointment()}}>Schedule Appointment</button>
+		</div>
+</div>	
+	
+	
+</div>
+</div>
+		</div>	
+			
+			
+			
+			
+	</div>
+		
+		
+		
+	</div>
+	</div>
+</div>
+
     </>)
 }
 
--- my-app/src/component/schedule_appointment/index.js
+++ my-app/src/component/schedule_appointment/index.js
@@ -77,87 +77,195 @@ const ScheduleAppointment = () => {
 }
 
     return (<>
-        <p> Schedule Appointment </p>
-        <div>
-            <div>
-                <p> Patient Information</p>
-                <div>
-                    <label>
-                        first Name:
-                        <input type="text" name="name" value={firstname} onChange={(e) => { setFirstname(e.target.value) }} />
-                    </label>
-                    <label>
-                        first  Name used:
-                        <input type="text" name="name" />
-                    </label>
-                    <label>
-                        Last  Name:
-                        <input type="text" value={lastname} onChange={(e) => { setLastname(e.target.value) }} name="name" />
-                    </label>
-                    <label>
-                        date of birth :
-                        <input type="date" name="name" value={dob} onChange={(e) => { setDob(e.target.value) }} />
-                    </label>
-                    <label>
-                        Sex
-                    </label>
-                    <select onChange={(e)=>setSex(e.target.value)} >
-                        <option>male</option>
-                        <option>female</option>
+               <div className="container-fluid">
+
+<div className="row">
+	<div className="col-md-10 col-12 mx-auto">
+		<div className="card mb-1">
+			<div className="card-header bg-primary text-white text-large">Schedule Appointment</div>
+
+</div>
+		<div className="row">
+	<div className="col-md-6">
+		<div className="card mb-5 ">
+			<div className="card-header text-medium bg-danger"><strong>Patient Information</strong></div>
+<div className="card-body">	
+	<div className="row">
+	<div className="col-md-6">
+		<div className="mb-3">
+<label className="form-label">First Name <span className="text-danger">*</span></label>
+{/* <select className="form-select">
+<option selected="selected">Choose...</option>
+<option>...</option>
+</select>		 */}
+   <input type="text" className="form-control" name="name" value={firstname} onChange={(e) => { setFirstname(e.target.value) }} />
+</div>
+		</div>
+		<div className="col-md-6">
+		<div className="mb-3">
+<label className="form-label">First Name - used</label>
+<input type="text" className="form-control"/>		
+</div>
+		</div>
+	</div>
+	<div className="row">	
+		<div className="col-12">
+		<div className="mb-3">
+<label className="form-label">Last Name <span className="text-danger">*</span></label>
+ <input type="text" className="form-control" value={lastname} onChange={(e) => { setLastname(e.target.value) }} name="name" />
+</div>
+		</div>
+	</div>
+	<div className="row">	
+		<div className="col-12">	
+			<label className="form-label">Date of birth <span className="text-danger">*</span></label>
+			<div className="input-group mb-3">
+            <input type="date" name="name" value={dob} className="form-control" onChange={(e) => { setDob(e.target.value) }} />
+				
+</div>			
+
+		</div>
+	</div>
+	<div className="row">	
+		<div className="col-12">
+		<div className="mb-3">
+<label className="form-label">Legal sex <span className="text-danger">*</span></label>
+
+<select className="form-select" onChange={(e)=>setSex(e.target.value)} >
+                        <option>Male</option>
+                        <option>Female</option>
 
                     </select>
-                    <label>
-                        Phone Required :
-                        <input type="number" name="name" value={phone} onChange={(e) => { setPhone(e.target.value) }} />
-                    </label>
-                    <label>
-                        phone type
-                    </label>
-                    <select >
-                        <option>data1</option>
-                        <option>data2</option>
-                        <option>data3</option>
-                        <option>data4</option>
-                    </select>
+</div>
+		</div>
+	</div>
+	<div className="row">	
+		<div className="col-md-6">
+		<div className="mb-3">
+<label className="form-label">Primary phone <span className="text-danger">*</span></label>
+<input className="form-control" type="number" name="name" value={phone} onChange={(e) => { setPhone(e.target.value) }} />		
+</div>
+		</div>
+		<div className="col-md-6">
+		<div className="mb-3">
+<label className="form-label">Phone type <span className="text-danger">*</span></label>
+<select className="form-select">
+<option selected="selected">Choose...</option>
+<option>...</option>
+</select>			
+</div>
+		</div>
+	</div>
+	<div className="row">	
+		<div className="col-12">
+		<div className="mb-3">
+<label className="form-label">Email <span className="text-danger">*</span></label>
+<input className="form-control" type="email" name="name" value={email} onChange={(e) => { setEmail(e.target.value) }} />		
+</div>
+		</div>
+	</div>
+</div>
+</div>
+		</div>
+			
+		<div className="col-md-6">
+		<div className="card mb-5 ">
+			<div className="card-header text-medium bg-danger"><strong>Appointment Details</strong></div>
+<div className="card-body">	
+	<div className="row mb-3">
+	<div className="col-md-6">
+		<div className="card h-100 hover-scale-up bg-light">
+<a className="card-body text-center d-flex flex-row align-items-center" href="#">
+<div className="sw-8 sh-8 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+	<img src="img/provider-black.png" alt=""/> </div>
+<p className="heading ps-3 text-body mb-0"><strong>{a.patientDetails.reason}</strong></p>
 
-                   
-                    <label>
-                        email Required :
-                        <input type="email" name="name" value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                    </label>
+</a>
+</div>
+		</div>
+	</div>
+	
+	<div className="row g-2 mb-3">
+<div className="col-xl-6 co-6">
+<div className="card border border-primary">
+<div className="card-body">
+<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-calendar-week"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Day and Time</strong></div>
+<div className="">{a.patientDetails.value}<br/> 9:20AM (EDT)</div>
 
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div className="col-xl-6 co-6">
+<div className="card border border-primary">
+<div className="card-body">
+<div className="row g-3">
+<div className="col-auto">
+<div className="sw-6 sh-6 rounded-xl d-flex justify-content-center align-items-center border border-primary">
+<i className="text-primary icon-20 bi-map-fill"></i>
+</div>
+</div>
+<div className="col">
+<div className="card-body d-flex flex-column ps-0 pt-0 pb-0 h-100 justify-content-center">
+<div className="d-flex flex-column">
+<div className="mb-0"><strong>Location</strong></div>
+<div className="">{a.patientDetails.location}</div>
 
-                </div>
-            </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+	<div className="row">	
+		<div className="col-12">
+		<div className="mb-3">
+<label className="form-label">Insurance <span className="text-danger">*</span></label>
+<input className="form-control" value={insurance} onChange={(e) => { setInsurance(e.target.value) }} />
+</div>
+			<div className="mb-3">
+<label className="form-label">Additional Notes <span className="text-danger">*</span></label>
+<input value={additional} className="form-control" onChange={(e) => { setAdditional(e.target.value) }}  />	
+</div>
+		</div>
+	</div>
+	<div className="row mt-3">	
+		<div className="col-12">
+			<button type="button" onClick={()=>{Preview()}} className="btn btn-primary mb-1 me-3">Previous</button>
+			<button type="button" onClick={()=>{ScheduleApi()}} className="btn btn-primary mb-1 me-3">Next</button>
+		</div>
+</div>	
+</div>
+</div>
+		</div>	
+			
+			
+			
+			
+	</div>
+		
+		
+		
+		
+		
+	</div>
+	</div>
+</div>
 
-
-
-            <div>
-                <p>Appointment Details</p>
-                <div>
-                    <p>{a.patientDetails.reason}</p>
-                    <div>
-                        <p> Day and Time</p>
-                        <p>{a.patientDetails.value}</p>
-                        <p> Location </p>
-
-                        <p> <p>{a.patientDetails.location}</p></p>
-                        <p>Insurance - Required</p>
-                        <input  value={insurance} onChange={(e) => { setInsurance(e.target.value) }} />
-                        <p>Additional notes - Required</p>
-                        <input value={additional} onChange={(e) => { setAdditional(e.target.value) }}  />
-
-
-                    </div>
-                </div>
-                <div>
-                    <button onClick={()=>{Preview()}}> Previous</button>
-                    <button onClick={()=>{ScheduleApi()}}> Next</button>
-                </div>
-
-            </div>
-
-        </div>
     </>)
 }
 
